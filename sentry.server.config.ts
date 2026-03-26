45e75d83821b531c2460c0fb96183d85bfd80caa
landells1 <jetrax20000@gmail.com>
2026-03-26 21:33:24 +0000

    Delete sentry.server.config.ts

--- sentry.server.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: 'https://813ce5394f6055a1b7989b4d3c2a012a@o4511112833925120.ingest.de.sentry.io/4511112837333072',
-  tracesSampleRate: 0.1,
-  debug: false,
-});
