175daa3317526cae7fc0d12c8ee035aadd54f192
landells1 <jetrax20000@gmail.com>
2026-03-26 21:11:50 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -5,4 +7,12 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, {
+  org: 'o4511112833925120',
+  project: 'medfolio',
+  silent: true,
+  telemetry: false,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+});
